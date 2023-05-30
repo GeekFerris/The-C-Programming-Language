51ec947a8a88588377800e034d4a5ece70abfc96
Ferris <ferris.debug@gmail.com>
2023-05-31 00:26:47 +0800

    4.3

---
+++ chapter04/4.3.c
@@ -0,0 +1,18 @@
+//
+// Created by Ferris on 2023/5/31.
+//
+
+#include "stdio.h"
+#include "stdlib.h"
+
+#define MAXOP 100
+#define NUMBER '0'
+
+int getop(char []);
+void push(double);
+double pop(void);
+
+int main()
+{
+    return 0;
+}
