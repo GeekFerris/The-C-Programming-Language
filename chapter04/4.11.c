1230e9d2582227c74d111d03d032d0c368569329
Ferris <ferris.debug@gmail.com>
2023-06-02 00:00:15 +0800

    4.11

---
+++ chapter04/4.11.c
@@ -0,0 +1,15 @@
+//
+// Created by Ferris on 2023/6/1.
+//
+
+// #include "文件名"
+// #include <文件名>
+
+#define 名字 替换文本
+/* 替换文本可以是任意的 */
+/* 无限循环 */
+#define forever for (;;)
+/* 也可以带参数 */
+#define max(A, B) ((A) > (B) ? (A) : (B))
+/* 取消宏定义 */
+#undef max(A, B)
