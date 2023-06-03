42a22ffbc57c8b35655305ae7a33b8730fa86fb4
小离 <438011578@qq.com>
2023-06-03 23:38:42 +0800

    5.6

--- chapter05/5.6.c
+++ chapter05/5.6.c
@@ -31,17 +31,25 @@ int get_line(char *, int);
 char *alloc(int);
 
 /* 读取输入行 */
-int read_lines(char *line_ptr[], int n_lines)
+int read_lines(char *line_ptr[], int max_lines)
 {
     int len, n_lines;
     char *p, line[MAX_LEN];
     n_lines = 0;
     while ((len = get_line(line, MAX_LEN)) > 0)
     {
-        if (n_lines >= MAX_LINES || (p = alloc(len)) == NULL) return -1;
+        if (n_lines >= max_lines || (p = alloc(len)) == NULL) return -1;
         line[len - 1] = '\0'; /* 删除换行符 */
         stpcpy(p, line);
         line_ptr[n_lines++] = p;
     }
     return n_lines;
+}
+
+/* 写输出行 */
+void write_lines(char *line_ptr[], int n_lines)
+{
+    int i;
+
+    for (i = 0; i < n_lines; ++i) printf("%s\n", line_ptr[i]);
 }
