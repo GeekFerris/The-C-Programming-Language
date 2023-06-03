1e72642c97c017690bd7829014e8b9788f7efeb6
小离 <438011578@qq.com>
2023-06-03 23:31:52 +0800

    5.6

--- chapter05/5.6.c
+++ chapter05/5.6.c
@@ -6,13 +6,22 @@
 
 #define MAX_LINES 5000 /* 进行排序的最大文本行数 */
 
-char *lineptr[MAX_LINES]; /* 指向文本行的指针数组 */
+char *line_ptr[MAX_LINES]; /* 指向文本行的指针数组 */
 
-int read_lines(char *lineprt[], int nlines);
-void writelines(char *linepts[], int nlines);
-void qsort(char *lineptr[], int nlines);
+int read_lines(char *line_ptr[], int n_lines);
+void write_lines(char *line_ptr[], int n_lines);
+void q_sort(char *line_ptr[], int left, int right);
 
 int main()
 {
-    return 0;
+    int n_lines; /* 读取的输入行数目 */
+
+    if ((n_lines = read_lines(line_ptr, MAX_LINES)) >= 0)
+    {
+        q_sort(line_ptr, 0, n_lines - 1);
+        write_lines(line_ptr, n_lines);
+        return 0;
+    }
+    printf("error: input too big to sort\n");
+    return 1;
 }
