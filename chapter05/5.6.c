c95b2f5e42de41d53ca6565542f3c87691adf789
小离 <438011578@qq.com>
2023-06-03 23:36:21 +0800

    5.6

--- chapter05/5.6.c
+++ chapter05/5.6.c
@@ -25,3 +25,23 @@ int main()
     printf("error: input too big to sort\n");
     return 1;
 }
+
+#define MAX_LEN 1000 /* 每个输入文本行的最大长度 */
+int get_line(char *, int);
+char *alloc(int);
+
+/* 读取输入行 */
+int read_lines(char *line_ptr[], int n_lines)
+{
+    int len, n_lines;
+    char *p, line[MAX_LEN];
+    n_lines = 0;
+    while ((len = get_line(line, MAX_LEN)) > 0)
+    {
+        if (n_lines >= MAX_LINES || (p = alloc(len)) == NULL) return -1;
+        line[len - 1] = '\0'; /* 删除换行符 */
+        stpcpy(p, line);
+        line_ptr[n_lines++] = p;
+    }
+    return n_lines;
+}
