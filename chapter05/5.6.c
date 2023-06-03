0a16cb6d078e3fbc127be7095ee10ddabe003a9a
小离 <438011578@qq.com>
2023-06-03 23:24:34 +0800

    5.6

--- chapter05/5.6.c
+++ chapter05/5.6.c
@@ -6,7 +6,7 @@
 
 #define MAX_LINES 5000 /* 进行排序的最大文本行数 */
 
-char *lineptr[MAX_LINES] /* 指向文本行的指针数组 */
+char *lineptr[MAX_LINES]; /* 指向文本行的指针数组 */
 
 int read_lines(char *lineprt[], int nlines);
 void writelines(char *linepts[], int nlines);
