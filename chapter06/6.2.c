1fdfc3d0e55f15c648685a6a8907a47f2e89ec8f
小离 <438011578@qq.com>
2023-06-04 23:31:29 +0800

    6.2

--- chapter06/6.2.c
+++ chapter06/6.2.c
@@ -13,8 +13,7 @@ struct rect {
 
 struct point make_point(int, int);
 
-struct point make_point(int x, int y)
-{
+struct point make_point(int x, int y) {
     struct point temp;
 
     temp.x = x;
@@ -25,9 +24,14 @@ struct point make_point(int x, int y)
 struct rect screen;
 struct point middle;
 
-struct point add_point(struct point p1, struct point p2)
-{
+/* 两个点相加 */
+struct point add_point(struct point p1, struct point p2) {
     p1.x += p2.x;
     p1.y += p2.y;
     return p1;
+}
+
+/* 如果点p在举行r内，则返回1，否则返回0 */
+int pt_in_rect(struct point p, struct rect r) {
+    return p.x >= r.pt1.x && p.x < r.pt2.x && p.y >= r.pt1.y && p.y < r.pt2.y;
 }
