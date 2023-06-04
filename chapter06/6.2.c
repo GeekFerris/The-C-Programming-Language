df3ead3756bc09441aa1236553b388af7b70c751
小离 <438011578@qq.com>
2023-06-04 23:29:06 +0800

    6.2

--- chapter06/6.2.c
+++ chapter06/6.2.c
@@ -6,6 +6,13 @@ struct point {
     int y;
 };
 
+struct rect {
+    struct point pt1;
+    struct point pt2;
+};
+
+struct point make_point(int, int);
+
 struct point make_point(int x, int y)
 {
     struct point temp;
@@ -13,4 +20,14 @@ struct point make_point(int x, int y)
     temp.x = x;
     temp.y = y;
     return temp;
+}
+
+struct rect screen;
+struct point middle;
+
+struct point add_point(struct point p1, struct point p2)
+{
+    p1.x += p2.x;
+    p1.y += p2.y;
+    return p1;
 }
