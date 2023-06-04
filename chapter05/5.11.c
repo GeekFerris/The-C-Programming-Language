bd6b04ae7acad8fe5c2f41ce73c0da7d76d66bad
小离 <438011578@qq.com>
2023-06-04 13:38:39 +0800

    5.11

---
+++ chapter05/5.11.c
@@ -0,0 +1,31 @@
+//
+// Created by Macintosh on 2023/6/4.
+//
+#include "stdio.h"
+#include "string.h"
+
+#define MAX_LINES 5000 /* 待排序的最大行数 */
+char *lineptr[MAX_LINES]; /* 指向文本行的指针 */
+
+int read_lines(char *line_prt[], int nlines);
+void write_lines(char *line_ptr[], int nlines);
+
+void qsort(void *line_prt[], int left, int right, int (*comp)(void *, void *));
+int numcmp(char *, char *);
+
+int main(int argc, char *argv[])
+{
+    int nlines; /* 读入的输入行数 */
+    int numeric = 0; /* 若进行数值排序 */
+
+    if (argc > 1 && strcmp(argv[1], "-n") == 0) numeric = 1;
+    if((nlines = read_lines(lineptr, MAX_LINES)) >= 0)
+    {
+        qsort((void **) lineptr, 0 , nlines - 1, (int (*)(void*, void *))(numeric ? numcmp : strcmp));
+        write_lines(lineptr, nlines);
+        return 0;
+    }
+
+    printf("input too big to sort\n");
+    return 1;
+}
