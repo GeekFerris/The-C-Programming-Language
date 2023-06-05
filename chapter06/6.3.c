5c6285c9caaca3450a5a41d6cafc5dd9ca8f9604
小离 <438011578@qq.com>
2023-06-05 23:46:06 +0800

    6.3

--- chapter06/6.3.c
+++ chapter06/6.3.c
@@ -5,7 +5,33 @@
 #include "ctype.h"
 #include "string.h"
 
+struct key {
+    char *word;
+    int count;
+};
+
+#define MAX_WORD 100
+/* c语言共32个关键字 */
+#define NKEYS 32
+
+struct key keytab[NKEYS];
+
+int get_word(char *, int);
+
+int bin_search(char *, struct key *, int);
+
+/* 统计c语言关键字的出现次数 */
 int main() {
+    int n;
+    char word[MAX_WORD];
+
+    while (get_word(word, MAX_WORD) != EOF) {
+        if (isalpha(word[0])) {
+            if ((n = bin_search(word, keytab, NKEYS) >= 0) {
+                keytab[n].count++;
+            }
+        }
+    }
 
     return 0;
 }
